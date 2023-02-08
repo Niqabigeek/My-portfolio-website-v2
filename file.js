338819ab5b4fcd879624d2640bacc67e853f0419
Zeniat Mohammed <zeniatmohammed10@gmail.com>
2023-02-08 09:56:26 +0100

    first commit

---
+++ file.js
@@ -0,0 +1,33 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+   
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    
+})
+
+document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+
+}))
+
+document.getElementById("button").addEventListener("click", function() {
+    window.location.href = "#portfolio";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
